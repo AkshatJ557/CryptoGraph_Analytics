b53fe3e157803606dfaf730c74cc14fe32f6e732
Priyansh Gadia <p.gadia@op.iitg.ac.in>
2026-06-14 18:15:01 +0530

    fix #4

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,8 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cryptograph.analytics',
   appName: 'CryptoGraph',
-  webDir: 'out',
-  bundledWebRuntime: false
+  webDir: 'out'
 };
 
 export default config;
